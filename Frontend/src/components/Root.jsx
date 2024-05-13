342235be4b67f3f2aebed1f9a47c75d4b22a4c13
ElgeneReyes <edreyes7@up.edu.ph>
2024-05-13 17:33:46 +0800

    feat: Added token expiration to handle JWT and improved signout and navbar for better security

--- Frontend/src/components/Root.jsx
+++ Frontend/src/components/Root.jsx
@@ -1,13 +1,46 @@
+import { useEffect } from 'react';
 import { Outlet, Link, useNavigate } from 'react-router-dom';
+import { jwtDecode } from 'jwt-decode';
 
 export default function Root() {
-    const isUserSignedIn = !!localStorage.getItem('token')
-    const navigate = useNavigate()
+    const isUserSignedIn = !!localStorage.getItem('token');
+    const navigate = useNavigate();
+
+    const isTokenExpired = (token) => {
+        if (!token) {
+            return true;
+        }
+        const decodedToken = jwtDecode(token);
+        if (!decodedToken || !decodedToken.exp) {
+            return true;
+        }
+        const currentTime = Math.floor(Date.now() / 1000);
+        return decodedToken.exp < currentTime;
+    };
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            handleTokenExpiration();
+        }, 60000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    const handleTokenExpiration = () => {
+        const token = localStorage.getItem('token');
+        if (isTokenExpired(token)) {
+            handleSignout();
+        }
+    };
 
     const handleSignout = () => {
-        localStorage.removeItem('token')
-        navigate('/login')
-    }
+        localStorage.removeItem('token');
+        localStorage.removeItem('cart');
+        localStorage.removeItem('email');
+        localStorage.removeItem('userType');
+        navigate('/login');
+    };
+    
     return (
         <>
             <nav className='navigation-bar'>
@@ -15,34 +48,53 @@ export default function Root() {
                     <li className='website-name'><Link to={`/`}>Farm to Shelf</Link></li>
                 </ul>
                 <ul className='ul-2'>
+                <>
+                    {isUserSignedIn ? (
+                    <>
+                        {localStorage.getItem('userType') === 'customer' && (
                         <>
-                            { isUserSignedIn ? (
-                                <>
-                                    <li><Link to={`/shop`}>Shop</Link></li>
-                                    <li><Link to={`/cart`}>Cart</Link></li>
-                                </>
-                            ) : (
-                                <>
-                                </>
-                            )}
+                            <li><Link to={`/shop`}>Shop</Link></li>
+                            <li><Link to={`/cart`}>Cart</Link></li>
                         </>
+                        )}
+                        {localStorage.getItem('userType') === 'merchant' && (
+                        <>
+                            <li><Link to={`/dashboard`}>Dashboard</Link></li>
+                            <li><Link to={`/merchantorders`}>Orders</Link></li>
+                        </>
+                        )}
+                    </>
+                    ) : (
+                    <>
+                    </>
+                    )}
+                </>
                 </ul>
                 <ul className='ul-3'>
+                <>
+                    {isUserSignedIn ? (
+                    <>
+                        {localStorage.getItem('userType') === 'customer' && (
                         <>
-                            { isUserSignedIn ? (
-                                <>
-                                    <li><Link to={`/account`}>Account</Link></li>
-                                    <li><Link to={`/orders`}>Orders</Link></li>
-                                    <li><button onClick={handleSignout}>Sign Out</button></li>
-                                </>
-                            ) : (
-                                <>
-                                    <li><Link to={`/login`}>Log In</Link></li>
-                                    <li><Link to={`/signup`}>Sign Up</Link></li>
-                                    <li><Link to={`/dashboard`}>Dashboard</Link></li>
-                                </>
-                            )}
+                            <li><Link to={`/account`}>Account</Link></li>
+                            <li><Link to={`/orders`}>Orders</Link></li>
+                            <li><button onClick={handleSignout}>Sign Out</button></li>
                         </>
+                        )}
+                        {localStorage.getItem('userType') === 'merchant' && (
+                        <>
+                            <li><Link to={`/account`}>Account</Link></li>
+                            <li><button onClick={handleSignout}>Sign Out</button></li>
+                        </>
+                        )}
+                    </>
+                    ) : (
+                    <>
+                        <li><Link to={`/login`}>Log In</Link></li>
+                        <li><Link to={`/signup`}>Sign Up</Link></li>
+                    </>
+                    )}
+                </>
                 </ul>
             </nav>
             <Outlet />
