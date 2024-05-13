2fad1e8d105f5f14eaede18fd236b14f4cd8727a
ElgeneReyes <edreyes7@up.edu.ph>
2024-05-13 17:24:08 +0800

    feat: Improved controller files for better data retrieval and error catch

--- Backend/controller/OrderController.js
+++ Backend/controller/OrderController.js
@@ -1,22 +1,74 @@
 import Order from '../model/OrderSchema.js';
+import Product from '../model/ProductSchema.js'
 import { v4 as uuidv4 } from 'uuid';
 
 export const placeOrder = async (req, res) => {
     try {
-        console.log('Request Body:', req.body);
+        console.log('Request Body:', req.body); // debug
 
-        const newOrder = new Order({
-            transactnID: uuidv4(),
-            productID: req.body.productID,
-            orderQty: req.body.orderQty,
-            orderStatus: 0,
-            email: req.body.email,
-        });
-        await newOrder.save();
-        console.log('Order placed successfully:', newOrder);
-        res.status(201).json({ message: 'Order placed successfully.' });
+        if (!Array.isArray(req.body) || req.body.length === 0) {
+            return res.status(400).json({ message: 'Invalid request body.' });
+        }
+
+        const saveTasks = [];
+
+        for (const item of req.body) {
+            const { productID, orderQty, email } = item;
+            if (!productID || !orderQty || !email) {
+                return res.status(400).json({ message: 'Missing required fields for an item.' });
+            }
+
+            const product = await Product.findOne({ productID });
+            if (!product) {
+                return res.status(404).json({ message: `Product with ID ${productID} not found.` });
+            }
+
+            const orderPrice = product.productPrice * orderQty;
+            const newOrder = new Order({
+                transactionID: uuidv4(),
+                productID,
+                orderQty,
+                orderStatus: 0,
+                email,
+                orderPrice,
+            });
+            
+            console.log('New Order: ', newOrder)
+            const saveTask = newOrder.save();
+            saveTasks.push(saveTask);
+        }
+
+        for (const saveTask of saveTasks) {
+            await saveTask;
+        }
+
+        console.log('Orders placed successfully');
+        res.status(201).json({ message: 'Orders placed successfully.' });
     } catch (err) {
-        console.error('Error placing order:', err);
+        console.error('Error placing orders:', err);
+        res.status(500).json({ message: 'Server Error' });
+    }
+};
+
+export const removeOrder = async (req, res) => {
+    try {
+        const transactionID = req.params.transactionID;
+        await Order.findOneAndDelete({ transactionID });
+        console.log('Order removed successfully:', transactionID);
+        res.json({ message: 'Order removed successfully.' });
+    } catch (err) {
+        console.error('Error removing order:', err);
+        res.status(500).json({ message: 'Server Error' });
+    }
+};
+
+export const getOrders = async (req, res) => {
+    try {
+        const orders = await Order.find();
+        console.log('All orders retrieved successfully:', orders);
+        res.json(orders);
+    } catch (err) {
+        console.error('Error getting orders:', err);
         res.status(500).json({ message: 'Server Error' });
     }
 };
--- Backend/controller/UserController.js
+++ Backend/controller/UserController.js
@@ -1,55 +1,66 @@
-/* eslint-disable no-unused-vars */
-import User from '../model/UserSchema.js'
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-import { v4 as uuidv4 } from 'uuid'
+import User from '../model/UserSchema.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { v4 as uuidv4 } from 'uuid';
 
-const secretKey = uuidv4()
+const secretKey = uuidv4();
 
 export const registerUser = async (req, res) => {
-    try {
-      const { firstName, middleName, lastName, email, password } = req.body
-      const hashedPassword = await bcrypt.hash(password, 10)
-      const userExists = await User.findOne({ email: req.body.email })
-      if (userExists) {
-        return res.json({ message: 'User already exists.' })
-      } else {
-        const newUser = new User({ firstName, middleName, lastName, userType: 'customer', email, password: hashedPassword })
-        await newUser.save()
-        res.status(201).json({ message: 'User registered successfully.' })
-      }
-    } catch (err) {
-      console.error(err)
-      res.status(500).json({ message: 'Error signing up.' })
+  try {
+    const { firstName, middleName, lastName, email, password, isMerchant } = req.body;
+    const hashedPassword = isMerchant ? password : await bcrypt.hash(password, 10);
+    const userExists = await User.findOne({ email: req.body.email });
+    if (userExists) {
+      return res.json({ message: 'User already exists.' });
+    } else {
+      const newUser = new User({ firstName, middleName, lastName, userType: isMerchant ? 'merchant' : 'customer', email, password: hashedPassword });
+      await newUser.save();
+      res.status(201).json({ message: 'User registered successfully.' });
     }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Error signing up.' });
   }
-  
-  
+};
+
 export const loginUser = async (req, res) => {
   try {
-    const { email, password } = req.body
-    const user = await User.findOne({ email })
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
     if (!user) {
-      return res.status(401).json({ error: 'Invalid credentials' })
+      return res.status(401).json({ error: 'Invalid credentials' });
     }
-    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    let isPasswordValid = false;
+
+    // Check if the user is a customer, then compare hashed password
+    if (user.userType === 'customer') {
+      isPasswordValid = await bcrypt.compare(password, user.password);
+    } else {
+      // For merchants or admins, directly compare the password
+      isPasswordValid = password === user.password;
+    }
+
     if (!isPasswordValid) {
-      return res.status(401).json({ error: 'Invalid credentials' })
+      return res.status(401).json({ error: 'Invalid credentials' });
     }
-    const token = jwt.sign({ userId: user._id }, secretKey, { expiresIn: '1hr' })
-    res.json({ message: 'Login successful' })
+
+    const token = jwt.sign({ userId: user._id, userType: user.userType }, secretKey, { expiresIn: '1h' });
+    res.json({ message: 'Login successful', token, userType: user.userType });
   } catch (err) {
-      res.status(500).json({ message: 'Error logging in' })
-    }
-}
-  
+    console.error('Error logging in:', err);
+    res.status(500).json({ message: 'Error logging in' });
+  }
+};
+
 export const getUsers = async (req, res) => {
-    try {
-        const users = await User.find({})
-        console.log('Users:', users)
-        res.json(users)
-    } catch (err) {
-        console.error('Error getting users:', err)
-        res.json({ message: 'Server Error' })
-    }
-}
+  try {
+    const users = await User.find({});
+    console.log('Users:', users);
+    res.json(users);
+  } catch (err) {
+    console.error('Error getting users:', err);
+    res.json({ message: 'Server Error' });
+  }
+};
